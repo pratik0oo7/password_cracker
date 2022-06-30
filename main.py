04a956003f9a346a7dd7f55903cf67feb078931c
Pratik Mendapara <65437627+pratik0oo7@users.noreply.github.com>
2022-06-30 12:22:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen, hashlib
 sha1hash = input("Please input the hash to crack.\n>")
-LIST_OF_COMMON_PASSWORDS = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'utf-8')
+LIST_OF_COMMON_PASSWORDS = str(urlopen('https://raw.githubusercontent.com/pratik0oo7/password_cracker/main/10-million-password-list-top-10000.txt').read(), 'utf-8')
 for guess in LIST_OF_COMMON_PASSWORDS.split('\n'):
     hashedGuess = hashlib.sha1(bytes(guess, 'utf-8')).hexdigest()
     if hashedGuess == sha1hash:
